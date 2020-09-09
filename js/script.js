ded5f208e465956071da7b373e238fead61d5ded
Valeria Curseri <curserivaleria@gmail.com>
2020-09-09 14:07:10 +0200

    File di base

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Completare l’esercizio iniziato a lezione sulla todo-list.
+// Utilizzare l’API di esempio http://157.230.17.132:3008
+// e fare le 4 operazioni Create, Read, Update e Delete.
+
+$(document).ready(function(){
+
+    
+
+})
