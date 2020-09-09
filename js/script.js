44ad5e02c83833ea89a3cc1b26bc2f1dd922f6ad
Valeria Curseri <curserivaleria@gmail.com>
2020-09-09 14:49:53 +0200

    Metodo GET

--- js/script.js
+++ js/script.js
@@ -4,6 +4,50 @@
 
 $(document).ready(function(){
 
-    
+    ottieniTodo();
+
+    // $('form#post-todo button').click(function(){
+    //     aggiungiTodo();
+    // });
+    //
+    // $('form#post-todo input').keyup(function(event){
+    //     if(event.keyCode == 13 || event.which == 13){
+    //         aggiungiTodo();
+    //     }
+    // });
 
 })
+
+// ---- funzioni ---- //
+
+function ottieniTodo(){
+    $.ajax(
+        {
+            url:'http://157.230.17.132:3008/todos',
+            method:'GET',
+            success: function(risposta){
+                stampaTodo(risposta);
+
+            },
+            error: function(){
+                alert('Si è verificato un errore');
+            }
+        }
+    )
+}
+
+function stampaTodo(data){
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    for (var i = 0; i < data.length; i++){
+        var context = {
+            todo: data[i].text
+        };
+        var html = template(context);
+        $('#to-dos').append(html);
+    }
+}
+
+// function aggiungiTodo(){
+//     var nuovoTodo = $('form#post-todo input').val();
+// }
